7f8c87e0bde3ca215ddbdc1bb34c0b1d0a11b9b4
Jakub <jakub.jakubecky123@gmail.com>
2020-10-14 23:13:57 +0200

    c

--- main.cpp
+++ main.cpp
@@ -1,26 +1,38 @@
 #include <iostream>
 #include <map>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 bool isInMap(const map<int, vector<string>> &hashMap, int key){
     return (hashMap.find(key) != hashMap.end());
 }
+bool vectorContains(vector<char> v, char k){
+    return (count(v.begin(), v.end(), k));
+}
+vector<string> castToSmall(vector<string> w){
+    for(int i = 0; i < w.size(); i ++){
+        for(int j = 0; j < w.at(i).size(); j ++){
+            w.at(i).at(j) = tolower(w.at(i).at(j));
+        }
+    }
+    return w;
+}
 struct RollingHash{
     map<int, vector<string>> hashMap;
     int author;
     RollingHash(vector<string> v, int author){
-        for(int i = 0; i < hashMap.size(); i ++){
+        for(int i = 0; i < v.size(); i ++){
             string word = v.at(i);
             int hash = getWordHash(word);
             if(!isInMap(hashMap, hash)){
-                hashMap.at(hash) = vector<string>();
+                hashMap[hash] = vector<string>();
             }
-            hashMap.at(hash).push_back(word);
+            hashMap[hash].push_back(word);
         }
         this->author = author;
     }
-    int getWordHash(string word){
+    static int getWordHash(string word){
         int hash = 0;
         for(int j = 0; j < word.size(); j ++){
             hash += word[j];
@@ -28,39 +40,51 @@ struct RollingHash{
         return hash;
     }
     bool isInHashMap(string word){
-        int hash = getWordHash(word);
-        if(isInMap(this->hashMap, hash)){
-            for(int i = 0; i < hashMap.at(hash).size(); i ++){
-                string s = hashMap.at(hash).at(i);
-                if(s == word) return true;
+        for (std::map<int, vector<string>>::iterator it = hashMap.begin(); it != hashMap.end(); it ++ ){
+            if(it->second.size() <= word.size()){
+                
             }
         }
-        return false;
     }
 };
 
-vector<string> split(string str, char divider){
+vector<string> split(string str, vector<char> divList){
     vector<string> result;
 
     string currWord = "";
     for(int i = 0; i < str.size(); i ++){
-        currWord+=str[i];
-        if(str[i] == divider || str.size()-1 == i){
+        if(vectorContains(divList, str[i])){
             result.push_back(currWord);
             currWord = "";
+        }else{
+            currWord+=str[i];
+        }
+        if(i+1 == str.size()){
+            result.push_back(currWord);
         }
     }
     return result;
 }
 
 int main() {
-    vector<string> mickiewicz = {"tadeusz","wojski","gerwazy","telimena","klucznik","podkomorz","wtenczas","litw","asesor","zosi","horeszkow","jac","soplic","maciej","soplic","moskal","protaz","dabrowski","strzelc","bernardyn","dobrzyns","biezy","chrzciciel","stolnik","charty","jegry","krzykneli","dobrzynski","mopanku","rzecze","kropiciel","kwestarz","rapier","bernardyna","buchman","konewka","kropic","hrabie","jankiel","karabele","klucznik","macka","majorze","moskal","moskala","polowanie","powiecie","scyzoryk","strzelby","chartow","dobrzynskich","dobyl","hejze","jegrow","litwa","mysliwi","stola","litewsk","prusak","ramion","rebajlo","rykow","szabli","szczwacze","zasciank","ascka","asesor","dobrzyn","drazki","dziedzic","konopie","kus","lowa","mysliwc","obadwa","ochmistrzyni","pleban","plut","podkomorz","ptastwo","skoluba","sokol","wloscian","wszerz","zareczyn","brzytew"};
-    RollingHash * rh = new RollingHash(mickiewicz, 0);
+    vector<string> mickiewicz = {"tadeusz","wojski","gerwaz","telimen","klucznik","podkomorz","wtencza","litw","asesor","zosi","horeszkow","jac","soplic","maciej","soplic","moskal","protaz","dabrowski","strzelc","bernardyn","dobrzyns","biezy","chrzciciel","stolnik","charty","jegry","krzykneli","dobrzynski","mopanku","rzecze","kropiciel","kwestarz","rapier","bernardyna","buchman","konewk","kropic","hrabi","jankiel","karabel","klucznik","macka","major","moskal","polowani","powie","scyzoryk","strzelb","chartow","dobrzynski","dobyl","hejze","jegrow","litw","mysliw","stola","litewsk","prusak","ramion","rebajlo","rykow","szabli","szczwacze","zasciank","ascka","asesor","dobrzyn","drazki","dziedzic","konopie","kus","lowa","mysliwc","obadwa","ochmistrzyni","pleban","plut","podkomorz","ptastwo","skoluba","sokol","wloscian","wszerz","zareczyn","brzytew"};
+    RollingHash * micRollingHash = new RollingHash(mickiewicz, 0);
     string line;
     getline(cin, line);
-    vector<string> args = split(line, ' ');
-    for(int i = 0; i < args.size(); i ++){
-        cout<<args.at(i);
+    vector<string> args = split(line, {' '});
+    int n = stoi(args[0]);
+    for(int i = 0; i < n; i ++){
+        getline(cin, line);
+        vector<string> words = split(line, {',','.', '\"', '\'', ';', ':','!','?', ' '});
+        words = castToSmall(words);
+        int mic = 0;
+        int sien = 0;
+        int prus = 0;
+        for(int i = 0; i < words.size(); i ++){
+            mic += micRollingHash->isInHashMap(words.at(i));
+           // cout<<words.at(i);
+        }
+        cout<<mic;
     }
     return 0;
 }
